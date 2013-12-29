82f8a563915a90c1f87e3f971289c06cf8670ed7
JJYung <jjyung1234@gmail.com>
2013-12-29 08:18:51 +0800

    follow function completed v1

--- src/com/airlocksoftware/hackernews/activity/FollowingActivity.java
+++ src/com/airlocksoftware/hackernews/activity/FollowingActivity.java
@@ -1,28 +1,102 @@
 // by jjyung 102522092
 package com.airlocksoftware.hackernews.activity;
 
+
+
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.os.Bundle;
+import android.os.Environment;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 import com.airlocksoftware.hackernews.R;
-import com.airlocksoftware.hackernews.interfaces.RestartableActivity;
+
 
 public class FollowingActivity extends SlideoutMenuActivity {
 	
-	RestartableActivity mRestart;
+	
+	ListView mList;
+	
+	String mFollowing[];
+	String SDpath = Environment.getExternalStorageDirectory().getPath();
+	String filename = "FollowSavedFile.txt";
+	
+	File mDir = new File(SDpath + "/HackerNews");
+	File mFile = new File(SDpath + "/HackerNews/" + filename);
+	
 	
 	@Override
 	public void onCreate(Bundle savedState) {
 		super.onCreate(savedState);
-		mRestart = this;
+		
 		setContentView(R.layout.act_following);
-
+		
+		
 		getActionBarView().getController()
 											.setTitleText(getString(R.string.following));
 		setActiveMenuItem(R.id.following_button);
+		
+		if(checkFile())
+			loadFollowing();
+		else
+			mFollowing = null;
+		listBinding();
+		
+	}
+	
+	private boolean checkFile(){
+		return (mDir.exists() && mFile.exists());
+	}
+	
+	private void loadFollowing(){
+	    StringBuilder sb = new StringBuilder();
+	    try {
+	        FileInputStream fin = new FileInputStream(mFile);
+	        byte[] data = new byte[fin.available()];
+	        while (fin.read(data) != -1) {
+	            sb.append(new String(data));
+	        }
+	        fin.close();
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    mFollowing = sb.toString().split("\n");
+	}
+
+	private void listBinding(){
+		mList = (ListView) findViewById(R.id.testlist);
+		
+		mList.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, mFollowing));
+		
+
+		mList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+		    public void onItemClick(AdapterView<?> adapter, View view, int position, long arg)   {
+				FollowingActivity activity = FollowingActivity.this;
+				UserActivity.startUserActivity(activity, mFollowing[position]);
+		    }
+		});
 	}
 	
 	
 	
+	private static final String[] mStrings = new String[] {
+		 "jjyung","abc","bbb","bcd","123","1234","test001","test002","test003","test004"
+		 };
 	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
